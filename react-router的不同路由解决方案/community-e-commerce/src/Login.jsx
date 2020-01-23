424a52ffaaeace3118205b84ac4ea758fb1affc2
waterwatermelon <1044264633@qq.com>
2020-01-23 23:59:40 +0800

    优化登录界面样式

--- react-router的不同路由解决方案/community-e-commerce/src/Login.jsx
+++ react-router的不同路由解决方案/community-e-commerce/src/Login.jsx
@@ -1,27 +1,39 @@
-import React,{Component} from 'react';
-
-class Login extends Component{
-    constructor(){
+import React, { Component } from 'react';
+import './style/style.css'
+class Login extends Component {
+    constructor() {
         super();
         this.state = {}
     }
     submitHandle = () => {
         console.log('this.props :', this.props);
-        sessionStorage.setItem("isLogin",true);
+        sessionStorage.setItem("isLogin", true);
         this.props.history.push('/user/main');
     }
-    render(){
+    render() {
         return (
             <div>
-                登录界面
-                <form onSubmit={this.submitHandle}>
-                    <p>账号：<input/></p>
-                    <p>密码：<input type="password"/></p>
-                    <button>登录</button>
-                </form>
+                <div className="lg-container">
+                    <div className="lg-title">登录</div>
+                    <div className="lg-box">
+                        <form onSubmit={this.submitHandle}>
+                            <div className="lg-row">
+                                <span className="lg-label">账号</span>
+                                <input type="text" className="lg-input" placeholder="请填写账户" />
+                            </div>
+                            <div className="lg-row">
+                                <span className="lg-label">密码</span>
+                                <input type="password" className="lg-input" placeholder="请填写密码" />
+                            </div>
+                            <div className="lg-ft">
+                                <button className="btn-block btn-lg">登录</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        );
-    }
-}
-
+                    );
+                }
+            }
+            
 export default Login;
