326694a09b105dacd80be42471a31e4ba447e90f
waterwatermelon <1044264633@qq.com>
2021-07-24 20:48:44 +0800

    [react][component]使用dumi和webpack编译组件库和组件库文档

---
+++ dumi-demo-webpack/src/Foo/index.js
@@ -0,0 +1,3 @@
+
+
+export default ({ title }) => <h1>{title}</h1>;
---
+++ dumi-demo-webpack/src/index.js
@@ -0,0 +1,4 @@
+export { default as Foo } from './Foo';
+import Button from './components/Button/Button.jsx';
+
+export { Button };
