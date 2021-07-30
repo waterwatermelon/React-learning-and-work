925d2b5a4747d2ef5aba38cafe1daf6e10e445f0
shifengjiao <shifengjiao@star-net.cn>
2021-07-30 18:04:45 +0800

    [css]添加tailwind实例

---
+++ react-tailwind/README.md
@@ -0,0 +1,1 @@
+[guides with create-react-app](https://www.tailwindcss.cn/docs/guides/create-react-app)
---
+++ react-tailwind/src/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    <div >
+      <div className='max-w-sm mx-auto bg-white shadow-md space-x-4'>
+        <span className='text-lg'>
+          hello
+      </span>
+        <span className='text-gray-500'>tailwind</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
