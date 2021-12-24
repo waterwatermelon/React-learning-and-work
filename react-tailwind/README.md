a8f5a94a5b2398e75b0f7a6099f78c681190ed8e
shifengjiao <shifengjiao@star-net.cn>
2021-12-24 10:01:52 +0800

    [tailwind]调整代码

--- react-tailwind/README.md
+++ react-tailwind/README.md
@@ -1,1 +1,5 @@
+tailwindcss 是一个构建定制用户界面的框架。
+
+[tailwindcss](https://www.tailwindcss.cn/)
+
 [guides with create-react-app](https://www.tailwindcss.cn/docs/guides/create-react-app)
--- react-tailwind/src/App.js
+++ react-tailwind/src/App.js
@@ -3,13 +3,21 @@
 
 function App() {
   return (
-    <div >
-      <div className='max-w-sm mx-auto bg-white shadow-md space-x-4'>
+    <div className='bg-gray-100 p-4'>
+      {/* header */}
+      <div className='max-w-sm mx-auto bg-white shadow-md space-x-4 p-2 rounded-lg'>
         <span className='text-lg'>
           hello
-      </span>
+        </span>
         <span className='text-gray-500'>tailwind</span>
+        <button className='p-2 rounded-md hover:bg-black hover:text-white' >button</button>
       </div>
+      {/* content TODO:布局 */}
+      <div className='bg-red-200 p-2 mt-2 rounded-xl'>
+          <img className='w-20 h-20 bg-black-800'/>
+          <div className=''>content</div>
+      </div>
+
     </div>
   );
 }
