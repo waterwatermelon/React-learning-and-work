f732a3d9423e5b24e0c7e006c9bb35a6647affca
shifengjiao <shifengjiao@star-net.cn>
2021-12-31 14:22:05 +0800

    [tailwind][mod]更新代码

--- react-tailwind/src/App.js
+++ react-tailwind/src/App.js
@@ -12,10 +12,24 @@ function App() {
       </div>
       {/* content TODO:布局 */}
       <div className='bg-red-200 p-2 mt-2 rounded-xl'>
-          <img className='w-20 h-20 bg-black-800'/>
-          <div className=''>content</div>
+        <img className='w-20 h-20 bg-black-800' />
+        <div className=''>content</div>
       </div>
 
+      <div className='mt-2'>
+        <label className='text-gray-600 mr-1'>label</label>
+        <input className='border-2 border-green-700 p-1 rounded-md focus:outline-none text-gray-700 '>
+        </input>
+      </div>
+
+      <div className='mt-2'>
+        <button className='bg-indigo-500 p-2 pt-1 pb-1 rounded-sm text-yellow-50 hover:cursor-pointer'>button</button>
+        <button className='btn'>button</button>
+      </div>
+      <select className='bg-white focus:outline-none' >
+        <option className=' '>1</option>
+        <option>2</option>
+      </select>
     </div>
   );
 }
