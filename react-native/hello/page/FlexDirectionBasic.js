fe8fd9b09c5763148e0d104c73adc34a2c09f7d2
shifengjiao <shifengjiao@star-net.cn>
2022-01-22 11:53:06 +0800

    [react-native][add] hello项目 1 使用flex布局(二）

--- react-native/hello/page/FlexDirectionBasic.js
+++ react-native/hello/page/FlexDirectionBasic.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -36,15 +36,55 @@ export default function FlexDirectionBasic() {
 
 function PreviewLayout(props) {
   const { children, } = props;
-  return (<View
-    style={[{
-      flex: 1,
+  const options = ['row', 'column', 'row-reverse', 'column-reverse'];
+  const [selectedValue, setSelectedValue] = useState(options[0]);
+  return (
+    <View style={{
       padding: 10,
       borderColor: 'black',
       borderWidth: 2,
-    }, {
-      // flexDirection: 'row',
-    }]}>
-    {children}
-  </View>);
+    }} >
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+      }}>
+
+        {
+          options.map(option => (
+            <Text
+              style={[{
+                width: '24%',
+                padding: 4,
+                justifyContent: 'center',
+                marginRight: '1%',
+                backgroundColor: 'white',
+                color: 'orange',
+                borderRadius: 4,
+                borderColor: 'orange',
+                borderWidth: 1,
+              },
+              option === selectedValue ? {
+                color: 'white',
+                backgroundColor: 'orange'
+              } : null]}
+              onPress={() => setSelectedValue(option)}
+            >
+              {option}
+            </Text>
+          ))
+        }
+      </View>
+      <View
+        style={[{
+          flex: 1,
+          padding: 10,
+          height: 200,
+          borderColor: 'steelblue',
+          borderWidth: 2,
+        }, {
+          flexDirection: selectedValue
+        }]}>
+        {children}
+      </View>
+    </View>);
 }
