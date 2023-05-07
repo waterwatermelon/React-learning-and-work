86af877b18d7c45dce9b132141b484ac70ab2aa3
watermelon <1044264633@qq.com>
2023-05-07 18:18:22 +0800

    [react-native][svg]使用svg文件（0.5h）

--- react-native/svg_demo/metro.config.js
+++ react-native/svg_demo/metro.config.js
@@ -4,14 +4,28 @@
  *
  * @format
  */
-
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+const { getDefaultConfig } = require('metro-config');
+// const {
+//   resolver: { sourceExts, assetExts },
+// } = await getDefaultConfig();
+async function getConfig() {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+          // babelTransformerPath: require.resolve('react-native-svg-transformer'),
+        },
+      }),
+    },
+    // resolver: {
+    //   assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    //   sourceExts: [...sourceExts, 'svg'],
+    // },
+  }
+}
+module.exports = getConfig();
