9d2c92561442ad16412214c40845e0030535a1bf
shifengjiao <shifengjiao@star-net.cn>
2023-05-25 08:38:34 +0800

    删除无用项目

--- react-native/hello/store/myStore.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage  from '@react-native-async-storage/async-storage';
-
-export function setData(key,value) {
-    AsyncStorage.setItem(key,value) 
-}
-
-export function getData(key) {
-   return AsyncStorage.getItem(key);
-}
